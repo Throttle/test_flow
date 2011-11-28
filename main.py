96aa54dac5273a1f17affeaae7b4ebc21be022a9
A.S <alexander.soulimov@gmail.com>
2011-11-29 01:27:53 +0400

    Launch main form from main script

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # coding: utf-8
+from PySide import QtGui
 import sys
+from forms.MainForm import Dialog
 
 __author__ = "Alexander Soulimov (alexander.soulimov@gmail.com)"
 __copyright__ = "Copyright (c) 2011 A.Soulimov"
@@ -11,7 +13,9 @@ def main(argv):
 	"""
 
 	"""
-	pass
+	app = QtGui.QApplication(sys.argv)
+	dialog = Dialog()
+	sys.exit(dialog.exec_())
 
 
 if __name__ == "__main__":
