19a54157534d479c32ed656eff0a1195d6cd874a
A.S <alexander.soulimov@gmail.com>
2011-11-29 00:46:15 +0400

    Added launcher

---
+++ main.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+import sys
+
+__author__ = "Alexander Soulimov (alexander.soulimov@gmail.com)"
+__copyright__ = "Copyright (c) 2011 A.Soulimov"
+__license__ = "Python"
+
+
+
+def main(argv):
+	"""
+
+	"""
+	pass
+
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
